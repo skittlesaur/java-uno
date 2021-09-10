c4b8ab1000556d3fb867babe76f30d2863c7e97e
baraa <baraa0ah@gmail.com>
2021-09-10 21:31:26 +0200

    documented title bar

--- src/dev/baraa/uno/graphics/TitleBar.java
+++ src/dev/baraa/uno/graphics/TitleBar.java
@@ -1,9 +1,17 @@
 package dev.baraa.uno.graphics;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TitleBar extends JPanel {
 
+    /**
+     * Generates the window's title bar.
+     * Instead of the old-styled Swing title bar, this gives the game a nicer feel when windowed.
+     */
     public TitleBar() {
+        setPreferredSize(new Dimension(0, 30));
+        setBackground(new Color(0x484848));
     }
+
 }
